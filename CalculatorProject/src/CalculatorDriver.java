b322a86867da1253f102874a4bb6df554f8b828a
NikeshCS <42121172+NikeshCS@users.noreply.github.com>
2019-01-31 22:44:51 -0600

    Add files via upload

---
+++ CalculatorProject/src/CalculatorDriver.java
@@ -0,0 +1,18 @@
+/* Windows 7 Calculator Project
+ * @Author: Nikesh Patel
+ * @Professor: Ziaullah Khan
+ */
+import javax.swing.JFrame;
+
+public class CalculatorDriver 
+{
+   public static void main(String[] args)
+   {
+       CalculatorMainFrame calculator = new CalculatorMainFrame();
+       calculator.setTitle("Calculator");
+       calculator.setSize(745,655);
+       calculator.setVisible(true);
+       calculator.setResizable(false);
+       calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+}
